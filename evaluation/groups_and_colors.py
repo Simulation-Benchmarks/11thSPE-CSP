43f1836b7b5bcf8dbb45cfd2892501ea0b89dc01
berndflemisch <bernd@iws.uni-stuttgart.de>
2025-01-30 12:52:15 +0000

    adapt line colors

--- evaluation/groups_and_colors.py
+++ evaluation/groups_and_colors.py
@@ -7,24 +7,24 @@ import matplotlib.pyplot as plt
 import seaborn
 
 groups_and_colors = {
-    "calgary": "#000000",
-    "cau-kiel": "#A30059",
-    "csiro": "#1B4400",
-    "ctc-cne": "#1CE6FF",
-    "darts": "#FF34FF",
-    "geos": "#FF4A46",
-    "ifpen": "#008941",
-    "kfupm": "#006FA6",
-    "opengosim": "#EFCBD5",#"#FFDBE5",
-    "opm": "#7A4900",
-    "pau-inria": "#0000A6",
-    "pflotran": "#63FFAC",
-    "rice": "#004D43",
-    "sintef": "#8FB0FF",
-    "slb": "#997D87",
-    "stuttgart": "#5A0007",
-    "tetratech": "#809693",
-    "ut-csee": "#4FC601"
+    "calgary": "#90B0DD",
+    "cau-kiel": "#C40F10",
+    "csiro": "#0B5197",
+    "ctc-cne": "#FFA052",
+    "darts": "#FF5A03",
+    "geos": "#B0B0B0",
+    "ifpen": "#000000",
+    "kfupm": "#FF7573",
+    "opengosim": "#AD92C3",
+    "opm": "#7141A3",
+    "pau-inria": "#AC7A71",
+    "pflotran": "#683229",
+    "rice": "#F39ABF",
+    "sintef": "#D651A9",
+    "slb": "#75D166",
+    "stuttgart": "#127F12",
+    "tetratech": "#07A4BB",
+    "ut-csee": "#A1A30C"
 }
 
 mass_cmap = matplotlib.colormaps['icefire'].resampled(256)
